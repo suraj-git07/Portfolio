fb362f39b40679471bd19ca6fd6092304b3d9728
Suraj Mishra <59327661+suraj-git07@users.noreply.github.com>
2022-09-30 11:47:51 +0530

    skill updated

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -16,7 +16,8 @@ const Skills = () => {
           <h3>Frameworks</h3>
 
           <p>
-            Truffle<br></br> Node<br></br>Web3 Js <br></br> React <br></br>
+            Truffle<br></br> Node<br></br>Web3 Js <br></br>Ether Js <br></br>{" "}
+            React <br></br>
             Openzeppelin
             <br></br>BOOTSTRAP
           </p>
