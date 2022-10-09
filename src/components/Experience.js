90424d3f050b8617bfda897ab53cedc6f7778ee2
Suraj Mishra <59327661+suraj-git07@users.noreply.github.com>
2022-10-10 01:05:58 +0530

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -15,6 +15,17 @@ const Experience = () => {
             <li>Help fellow students by mentoring them about web3</li>
           </ul>
         </li>
+       <li>
+          <i>
+            InfoXpression University School of Information, Communication and Technology
+          </i>{" "}
+          - <strong>Frontend Developer</strong>, 2022-Present
+          <ul>
+            <li>Created multiple scalable and reusable react components for InfoXpression website(The annual techfest of USICT)</li>
+            <li>Skills: JavaScript React.js  Figma (Software)  GitHub  Web Design  Teamwork</li>
+            <li>visit website at: "https://Infoxpression.tech/" </li>
+          </ul>
+        </li>
       </ul>
     </div>
   );
