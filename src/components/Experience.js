5df5d217bf05840b0d02b8ebc04224b0e601ace6
Suraj Mishra <59327661+suraj-git07@users.noreply.github.com>
2023-03-22 04:17:26 +0530

    updates sdc experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,15 +4,27 @@ const Experience = () => {
       <h2>Experience</h2>
 
       <ul>
-        <li>
+    <li>
+          <i>
+            Software Devlopement Cell, University School of Information,
+            Communication and Technology
+          </i>{" "}
+          - <strong>DevRel</strong>, 202-Present
+          <ul>
+            <li>Organized events on web3 and blockchain</li>
+            <li>Help fellow students by mentoring them about web3</li>
+          </ul>
+        </li>
+       <li>    
+    <li>
           <i>
             Google Developer Student Clubs, University School of Information,
             Communication and Technology
           </i>{" "}
-          - <strong>Web3 Developer</strong>, 2022-Present
+          - <strong>Web3 Developer</strong>, 2023-Present
           <ul>
-            <li>Organized events on web3 and blockchain</li>
-            <li>Help fellow students by mentoring them about web3</li>
+            <li>Initially Started as a web3 developer at SDC GGSIPU and in this new tenure at SDC, got the opportunity to use my communication and managing people skills to work with and for great devs and help SDC to grow to new heights.</li>
+            
           </ul>
         </li>
        <li>
