fe0e84b8757c72adde58b625c73d2d2504f8588f
Suraj Mishra <59327661+suraj-git07@users.noreply.github.com>
2023-03-22 04:25:54 +0530

    corrected the error

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -11,11 +11,10 @@ const Experience = () => {
           </i>{" "}
           - <strong>DevRel</strong>, 202-Present
           <ul>
-            <li>Organized events on web3 and blockchain</li>
-            <li>Help fellow students by mentoring them about web3</li>
+            <li>Initially Started as a web3 developer at SDC GGSIPU and in this new tenure at SDC, got the opportunity to use my communication and managing people skills to work with and for great devs and help SDC to grow to new heights.</li>
           </ul>
         </li>
-       <li>    
+   
     <li>
           <i>
             Google Developer Student Clubs, University School of Information,
@@ -23,8 +22,9 @@ const Experience = () => {
           </i>{" "}
           - <strong>Web3 Developer</strong>, 2023-Present
           <ul>
-            <li>Initially Started as a web3 developer at SDC GGSIPU and in this new tenure at SDC, got the opportunity to use my communication and managing people skills to work with and for great devs and help SDC to grow to new heights.</li>
             
+            <li>Organized events on web3 and blockchain</li>
+            <li>Help fellow students by mentoring them about web3</li>
           </ul>
         </li>
        <li>
